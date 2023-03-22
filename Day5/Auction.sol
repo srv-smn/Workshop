ae03e61003b8192790c6f9ee106a44f7f817fe62
Sourav Suman <47235134+srv-smn@users.noreply.github.com>
2023-03-22 10:13:04 +0530

    Update Auction.sol

--- Day5/Auction.sol
+++ Day5/Auction.sol
@@ -3,39 +3,96 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-
 interface IERC721 {
-    function safeTransferFrom(address from, address to, uint tokenId) external;
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
 
-    function transferFrom(address, address, uint) external;
+    function transferFrom(
+        address,
+        address,
+        uint256
+    ) external;
 }
 
 contract realstate {
-    constructor() {
-        // erc20 address
-        //erc721 address
-        //nft id
-        // highest bid
-        // seller
+    IERC20 erc20;
+    IERC721 erc721;
+    uint256 nftid;
+    uint256 highestbid;
+    address seller;
+    bool started;
+    address highestBidder;
+    uint256 endtime;
+    mapping(address => uint256) public bids;
 
+    constructor(
+        address _erc20,
+        address _erc721,
+        uint256 _nftid,
+        uint256 _highestBid
+    ) {
+        // erc20 address
+
+        erc20 = IERC20(_erc20);
+
+        //erc721 address
+        erc721 = IERC721(_erc721);
+        //nft id
+        nftid = _nftid;
+        // highest bid
+        highestbid = _highestBid;
+        // seller
+        seller = msg.sender;
     }
 
     function start() external {
+        require(!started, "alreadstarted");
+        require(msg.sender == seller, "You are not seller.");
+        erc721.transferFrom(msg.sender, address(this), nftid);
+        started = true;
+        endtime = block.timestamp + 2 days;
+
         // endtime
         // transfer NFT
         // start ?
     }
 
-    function bid(uint _amount) external {
+    function bid(uint256 _amount) external {
+        //start or not
+        require(started, "not started");
+        require(block.timestamp < endtime, "ended");
+        if (highestBidder != address(0)) {
+            bids[highestBidder] += highestbid;
+        }
+
         // highest bidder
+        require(_amount > highestbid, "you cannot bid");
+        highestbid = _amount;
+
+        highestBidder = msg.sender;
         // erc20 transfer
+        erc20.transferFrom(msg.sender, address(this), _amount);
     }
 
     function withdraw() external {
         // token transer to user
+        require(highestBidder != msg.sender, "highestBidder");
+        uint256 bal = bids[msg.sender];
+        bids[msg.sender] = 0;
+        erc20.transfer(msg.sender, bal);
     }
 
     function end() external {
-        // highest bidder - erc721 transfer 
+        // highest bidder - erc721 transfer
+        require(block.timestamp > endtime, "not ended");
+        if (highestBidder != address(0)) {
+            erc721.safeTransferFrom(address(this), highestBidder, nftid);
+            erc20.transfer(seller, highestbid);
+        } else {
+            erc721.safeTransferFrom(address(this), seller, nftid);
+        }
     }
 }
